c652f14b120461df263fde35a57bb30826dbbe15
Øystein Ø. Olsen <fireneslo@gmail.com>
2016-10-13 00:01:07 +0200

    build

--- rollup.config.js
+++
@@ -1,8 +0,0 @@
-
-export default {
-  targets: [
-    { dest: 'dist/bundle.cjs.js', format: 'cjs' },
-    { dest: 'dist/bundle.umd.js', format: 'umd' },
-    { dest: 'dist/bundle.es.js', format: 'es' },
-  ]
-};
